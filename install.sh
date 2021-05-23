fe959cba41861212d78c4f1c2284be2ac0ab513e
facila <facila@gmx.fr>
2021-05-23 17:58:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+# version 1.00 Mai 2021
 # se positionner dans le répertoire contenant install.sh et le fichier tar.gz
-# exécuter la commande : sudo sh install.sh
+# exécuter la commande : sudo sh install.sh APPLI VERSION
 
-APPLI=kreo
-VERSION=0.02
+APPLI=$1
+VERSION=$2
 
 DIR=/usr/local/facila
 LG=fr_FR.UTF-8
 
-[ "`whoami`" != 'root' ] && { echo vous devez exécuter : sudo sh install.sh ; exit ; }
+[ "`whoami`" != 'root' ] && { echo vous devez exécuter : sudo sh install.sh APPLI VERSION ; exit ; }
+
+proc_exit ()
+{
+perl -e "$1" 2>/dev/null
+[ $? != "0" ] && { echo "vous devez d'abbord installer : $2" ; exit ; }
+}
 
 # vérification des dépendances
-perl -e '' 2>/dev/null ; [ $? != "0" ] && { echo "vous devez d'abbord installer : perl" ; exit ; }
-
-ERROR=''
-perl -e 'use Tk' 2>/dev/null ; [ $? != "0" ] && ERROR=$ERROR"perl-tk"
-[ "$ERROR" != '' ] && { echo "vous devez d'abbord installer : $ERROR" ; exit ; }
+proc_exit ''       perl 
+proc_exit 'use Tk' perl-tk
+[ "$APPLI" = 'kalkulo' ] { proc_exit 'use Net::Kalk' facila/Net-Kalk }
 
 FILE=$APPLI.$VERSION.tar.gz
+[ -s $FILE ] && { echo fichier $FILE absent ; exit ; }
 echo installation de facila $FILE
 tar -xzf $FILE -C /
 
